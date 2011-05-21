b8df3ccd03d9475378b0b962cf163b4fe802b054
Sean <sean.keevey@gmail.com>
2011-05-21 14:36:35 +0100

    First commit

---
+++ functions.js
@@ -0,0 +1,12 @@
+﻿setInterval("reloadImg('radar')", 600000);
+function reloadImg(id) {
+   var obj = document.getElementById(id);
+   var src = obj.src;
+   var pos = src.indexOf('?');
+   if (pos >= 0) {
+      src = src.substr(0, pos);
+   }
+   var date = new Date();
+   obj.src = src + '?v=' + date.getTime();
+   return false;
+}
